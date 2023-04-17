f1e7a6c49b613bc6dc858fdbe1c1ef9877ce702a
Gabriella Romero <rgabriella752@gmail.com>
2023-04-16 21:38:24 -0700

    upload changes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(hobby = "roller-skate") {
+     return (`This Saturday, I want to ${hobby}!`)
+}
+
+const mondayWork = function(work = "go to the office") {
+     return (`This Monday, I will ${work}.`) ;
+}
+function wrapAdjective(string1 = "*") {
+     return function (value1 = "special") {
+          return (`You are ${string1}${value1}${string1}!`)
+     }
+}
+wrapAdjective("||")("a dedicated programmer")
